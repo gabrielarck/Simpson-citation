74fcfdecaed9194c46d0dad4b07de718fcca2fdd
gabrielarck <stanislasvega@yahoo.fr>
2019-10-28 11:22:13 +0100

    add modification for begin

---
+++ src/components/AppelApi.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+
+function AppelApi (){
+    return(
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default AppelApi; 
